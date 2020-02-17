c264eecccf179417e8c40aa78395b33e3c3c5420
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-02-17 11:15:25 -0600

    Componente Lista
    Componente Lista

--- blapp/src/app/components/professor/list/list.component.ts
+++ blapp/src/app/components/professor/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
+  @Input() nombre: string;
   constructor() { }
 
   ngOnInit() {}
