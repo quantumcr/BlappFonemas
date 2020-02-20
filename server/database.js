60dc25600527bccee728df7a41316a866ee6c526
Geovanny Francisco <geoburgosretana@gmail.com>
2020-02-20 15:49:02 -0600

    Lista estudiantes frotend

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//const URI = 'mongodb+srv://amarrero:MilitaryIntelligenceSection06@teca-7rrky.mongodb.net/teca?retryWrites=true&w=majority';
+//const URI = 'mongodb+srv://amarrero:MilitaryIntelligenceSection06@teca-7rrky.mongodb.net/blapp?retryWrites=true&w=majority';
 const URI = 'mongodb://localhost:27017/blapp';
 
 mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
