6c0b3b9570f28be6f66d4bd50880fe3ef4b17693
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-02-23 10:12:31 -0600

    Actualizacion del servidor
    Cambio de base de datos a Mongo Atlas

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const URI = 'mongodb+srv://amarrero:MilitaryIntelligenceSection06@teca-7rrky.mongodb.net/teca?retryWrites=true&w=majority';
-const URI = 'mongodb://localhost:27017/blapp';
+const URI = 'mongodb+srv://amarrero:MilitaryIntelligenceSection06@teca-7rrky.mongodb.net/blapp?retryWrites=true&w=majority';
+//const URI = 'mongodb://localhost:27017/blapp';
 
 mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
     .then(db => console.log('DB is connected'))
