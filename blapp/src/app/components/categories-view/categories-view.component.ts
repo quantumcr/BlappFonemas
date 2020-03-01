7bf2f41ab0f30b6fa9357f87ae1ad1977bbd3c1d
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-01 07:28:02 -0600

    Categories Component
    Creación del componente para las categorías semánticas

---
+++ blapp/src/app/components/categories-view/categories-view.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-view',
+  templateUrl: './categories-view.component.html',
+  styleUrls: ['./categories-view.component.scss'],
+})
+export class CategoriesViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
