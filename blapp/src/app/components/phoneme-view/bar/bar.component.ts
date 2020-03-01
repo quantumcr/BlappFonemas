13bb3b6160ce147b38f38861fd226bf30e9fb1d0
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-01 07:33:15 -0600

    Phoneme View
    Modificaciones al componente

--- blapp/src/app/components/phoneme-view/bar/bar.component.ts
+++ blapp/src/app/components/phoneme-view/bar/bar.component.ts
@@ -9,14 +9,29 @@ export class BarComponent implements OnInit {
   @Input() fonema: string;
   @Input() mensaje: string;
   @Output() barEvent = new EventEmitter();
+  public visibleAudio = false;
+  public visibleVideo = true;
   constructor() { }
 
   ngOnInit() {}
   openPanel(event: any) {
     console.log(event.target.value);
     this.barEvent.emit(event.target.value);
+    this.setVisible(event.target.value);
+
   }
 
   playVideo() {}
+    setVisible(value: string) {
+      if ( value === 'video') {
+        this.visibleVideo = false;
+        this.visibleAudio = true;
+      }
+      if (value === 'phoneme') {
+        this.visibleVideo = true;
+        this.visibleAudio = false;
+      }
+
+    }
 
 }
