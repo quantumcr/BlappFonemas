616818202be27bf7f2e94747680caeab3985544d
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-01 07:27:13 -0600

    Categories view panel
    Componente para las categorías semánticas

---
+++ blapp/src/app/components/categories-view/categories-view-panel/categories-view-panel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-view-panel',
+  templateUrl: './categories-view-panel.component.html',
+  styleUrls: ['./categories-view-panel.component.scss'],
+})
+export class CategoriesViewPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
