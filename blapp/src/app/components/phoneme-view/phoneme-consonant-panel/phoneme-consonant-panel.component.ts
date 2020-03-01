fad6ba0cc79eb8b784161d32f3dd75be329b3e9b
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-01 07:29:55 -0600

    Consonant Panel
    Modificaciones al panel de consonantes

---
+++ blapp/src/app/components/phoneme-view/phoneme-consonant-panel/phoneme-consonant-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phoneme-consonant-panel',
+  templateUrl: './phoneme-consonant-panel.component.html',
+  styleUrls: ['./phoneme-consonant-panel.component.scss'],
+})
+export class PhonemeConsonantPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+  playAudio(event: any) {}
+
+}
