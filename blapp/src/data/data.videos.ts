97899c3c27cb8b9f00f2e6a0d4d8c4f75481a36c
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-01 07:36:02 -0600

    Modificaciones
    Audio y drag and drop

---
+++ blapp/src/data/data.videos.ts
@@ -0,0 +1,156 @@
+export const VIDEOS = [
+    {
+        nombre: 'Aa',
+        video: 'assets/video/fonemas/vidFonA.mp4',
+        imagen: 'assets/img/fonemaA.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Bb',
+        video: 'assets/video/fonemas/vidFonB.mp4',
+        imagen: 'assets/img/fonemaB.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Ch',
+        video: 'assets/video/fonemas/vidFonCh.mp4',
+        imagen: 'assets/img/fonemaCh.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Dd',
+        video: 'assets/video/fonemas/vidFonD.mp4',
+        imagen: 'assets/img/fonemaD.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Ee',
+        video: 'assets/video/fonemas/vidFonE.mp4',
+        imagen: 'assets/img/fonemaE.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Ff',
+        video: 'assets/video/fonemas/vidFonF.mp4',
+        imagen: 'assets/img/fonemaF.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Gg',
+        video: 'assets/video/fonemas/vidFonG.mp4',
+        imagen: 'assets/img/fonemaG.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Ii',
+        video: 'assets/video/fonemas/vidFonI.mp4',
+        imagen: 'assets/img/fonemaI.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Jj',
+        video: 'assets/video/fonemas/vidFonJ.mp4',
+        imagen: 'assets/img/fonemaJ.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Kk',
+        video: 'assets/video/fonemas/vidFonK.mp4',
+        imagen: 'assets/img/fonemaK.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Ll',
+        video: 'assets/video/fonemas/vidFonL.mp4',
+        imagen: 'assets/img/fonemaL.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Mm',
+        video: 'assets/video/fonemas/vidFonM.mp4',
+        imagen: 'assets/img/fonemaM.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Nn',
+        video: 'assets/video/fonemas/vidFonNn.mp4',
+        imagen: 'assets/img/fonemaNn.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Nñ',
+        video: 'assets/video/fonemas/vidFonÑ.mp4',
+        imagen: 'assets/img/fonemaÑ.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Oo',
+        video: 'assets/video/fonemas/vidFonO.mp4',
+        imagen: 'assets/img/fonemaO.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Pp',
+        video: 'assets/video/fonemas/vidFonP.mp4',
+        imagen: 'assets/img/fonemaP.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Rr',
+        video: 'assets/video/fonemas/vidFonRr.mp4',
+        imagen: 'assets/img/fonemaRr.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'RR',
+        video: 'assets/video/fonemas/vidFonRR.mp4',
+        imagen: 'assets/img/fonemaRR.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Ss',
+        video: 'assets/video/fonemas/vidFonS.mp4',
+        imagen: 'assets/img/fonemaS.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Tt',
+        video: 'assets/video/fonemas/vidFonT.mp4',
+        imagen: 'assets/img/fonemaT.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Xx',
+        video: 'assets/video/fonemas/vidFonX.mp4',
+        imagen: 'assets/img/fonemaX.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+    {
+        nombre: 'Yy',
+        video: 'assets/video/fonemas/vidFonY.mp4',
+        imagen: 'assets/img/fonemaY.svg',
+        duracion: 4,
+        reproduciendo: false
+    },
+];
