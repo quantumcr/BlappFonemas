afd2adb8c60d34de3d290eb55c9100bd315af218
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-01 07:35:41 -0600

    Update
    trabajo base para actualizar

--- blapp/src/app/pages/semanticas/semanticas.page.ts
+++ blapp/src/app/pages/semanticas/semanticas.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DragulaService } from 'ng2-dragula';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-semanticas',
   templateUrl: './semanticas.page.html',
   styleUrls: ['./semanticas.page.scss'],
 })
 export class SemanticasPage implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
+
+
 
 }
