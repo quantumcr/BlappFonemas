d708fe58a5a608555a11ac4968bcfab4c0bef4a9
Geovanny Francisco <geoburgosretana@gmail.com>
2020-03-13 17:21:41 -0600

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const URI = process.env.API_URL;
 //const URI = process.env.API_URL_LOCAL;
-console.log(URI);
+
 mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
     .then(db => console.log('DB is connected'))
-    .catch(err => console.error("Error db connected: ", err));
+    .catch(err => console.error(err));
 
 module.exports = mongoose;
