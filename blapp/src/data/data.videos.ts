72f237231cd74494f52080447196cbb48fdf1f84
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-03-15 18:00:12 -0600

    Update Fonemas
    Actualizacion de fonemas

--- blapp/src/data/data.videos.ts
+++ blapp/src/data/data.videos.ts
@@ -4,7 +4,7 @@ export const VIDEOS = [
     {
         nombre: 'a',
         video: 'assets/video/fonemas/vidFonA.mp4',
-        imagen: 'assets/img/fonemaA.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaA.png',
         mensaje: 'Aaaaaaaa (Sirena de ambulancia)',
         duracion: 4,
         reproduciendo: false
@@ -12,7 +12,7 @@ export const VIDEOS = [
     {
         nombre: 'b',
         video: 'assets/video/fonemas/vidFonB.mp4',
-        imagen: 'assets/img/fonemaB.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaB.png',
         mensaje: 'Beeeee Beeeee (Oveja)',
         duracion: 4,
         reproduciendo: false
@@ -20,7 +20,7 @@ export const VIDEOS = [
     {
         nombre: 'c',
         video: 'assets/video/fonemas/vidFonC.mp4',
-        imagen: 'assets/img/fonemaC.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaC.png',
         mensaje: 'Cuac Cuac Cuac (Pato)',
         duracion: 4,
         reproduciendo: false
@@ -28,7 +28,7 @@ export const VIDEOS = [
     {
         nombre: 'ch',
         video: 'assets/video/fonemas/vidFonCh.mp4',
-        imagen: 'assets/img/fonemaCh.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaCh.png',
         mensaje: 'Chuuuu Chuuu Chuuu (Trencito)',
         duracion: 4,
         reproduciendo: false
@@ -36,7 +36,7 @@ export const VIDEOS = [
     {
         nombre: 'd',
         video: 'assets/video/fonemas/vidFonD.mp4',
-        imagen: 'assets/img/fonemaD.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaD.png',
         mensaje: 'Dddddddddd (Abejas cerca de panal)',
         duracion: 4,
         reproduciendo: false
@@ -44,7 +44,7 @@ export const VIDEOS = [
     {
         nombre: 'e',
         video: 'assets/video/fonemas/vidFonE.mp4',
-        imagen: 'assets/img/fonemaE.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaE.png',
         mensaje: 'Ehhh Ehhh Ehhh (Alegría)',
         duracion: 4,
         reproduciendo: false
@@ -52,7 +52,7 @@ export const VIDEOS = [
     {
         nombre: 'f',
         video: 'assets/video/fonemas/vidFonF.mp4',
-        imagen: 'assets/img/fonemaF.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaF.png',
         mensaje: 'Fiiii Fiiii Fiiii (Soplando flauta)',
         duracion: 4,
         reproduciendo: false
@@ -68,7 +68,7 @@ export const VIDEOS = [
     {
         nombre: 'i',
         video: 'assets/video/fonemas/vidFonI.mp4',
-        imagen: 'assets/img/fonemaI.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaI.png',
         mensaje: 'Iiiii Iiiii Iiiii (Ratoncito)',
         duracion: 4,
         reproduciendo: false
@@ -76,7 +76,7 @@ export const VIDEOS = [
     {
         nombre: 'j',
         video: 'assets/video/fonemas/vidFonJ.mp4',
-        imagen: 'assets/img/fonemaJ.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaJ.png',
         mensaje: 'Jajajajajajaja (risas)',
         duracion: 4,
         reproduciendo: false
@@ -108,7 +108,7 @@ export const VIDEOS = [
     {
         nombre: 'm',
         video: 'assets/video/fonemas/vidFonM.mp4',
-        imagen: 'assets/img/fonemaM.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaM.png',
         mensaje: 'Muuu Muuu Muuu (Vaca)',
         duracion: 4,
         reproduciendo: false
@@ -116,7 +116,7 @@ export const VIDEOS = [
     {
         nombre: 'n',
         video: 'assets/video/fonemas/vidFonNn.mp4',
-        imagen: 'assets/img/fonemaNn.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaN.png',
         mensaje: 'Ni No Ni No Ni No (Minions)',
         duracion: 4,
         reproduciendo: false
@@ -124,7 +124,7 @@ export const VIDEOS = [
     {
         nombre: 'ñ',
         video: 'assets/video/fonemas/vidFonÑ.mp4',
-        imagen: 'assets/img/fonemaÑ.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaÑ.png',
         mensaje: 'Ñooo Ñooo Ñooo (Chanchito)',
         duracion: 4,
         reproduciendo: false
@@ -156,15 +156,15 @@ export const VIDEOS = [
     {
         nombre: 'rr',
         video: 'assets/video/fonemas/vidFonRR.mp4',
-        imagen: 'assets/img/fonemaRR.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaRR.png',
         mensaje: 'Rrrrrrrrrrrr (Moto)',
         duracion: 4,
         reproduciendo: false
     },
     {
-        nombre: 'Ss',
+        nombre: 's',
         video: 'assets/video/fonemas/vidFonS.mp4',
-        imagen: 'assets/img/fonemaS.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaSs.png',
         mensaje: 'Sssssssss (Serpiente)',
         duracion: 4,
         reproduciendo: false
@@ -188,7 +188,7 @@ export const VIDEOS = [
     {
         nombre: 'x',
         video: 'assets/video/fonemas/vidFonX.mp4',
-        imagen: 'assets/img/fonemaX.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaX.png',
         mensaje: 'Xxxx Xxxx Xxxx (spray)',
         duracion: 4,
         reproduciendo: false
@@ -196,7 +196,7 @@ export const VIDEOS = [
     {
         nombre: 'y',
         video: 'assets/video/fonemas/vidFonY.mp4',
-        imagen: 'assets/img/fonemaY.svg',
+        imagen: 'assets/img/fonemas/phoneme-bar/fonemaY.png',
         mensaje: 'Yyyyyyyy (Avión volando)',
         duracion: 4,
         reproduciendo: false
