54969797bafd78de9933a318f3c71302d78098cb
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2020-07-05 17:03:41 -0600

    Update auth.service.ts
    Actualizacion del Auth Service

--- blapp/src/app/services/auth.service.ts
+++ blapp/src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
+import {AngularFirestore} from '@angular/fire/firestore';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private AFauth: AngularFireAuth, public router: Router) { }
+  constructor(private AFauth: AngularFireAuth, public router: Router, private db: AngularFirestore) { }
   // Ingreso a la aplicación
   login(email: string, password: string) {
     return new Promise((resolve, rejected) => {
@@ -21,5 +23,24 @@ export class AuthService {
     this.AFauth.signOut().then(auth =>{
       this.router.navigate(['/inicio']);
     })
+  }
+
+  register(nombre: string, institucion: string, email: string, password: string) {
+    return new Promise ((resolve, rejected) => {
+      this.AFauth.createUserWithEmailAndPassword(email, password).then(res=>{
+        const uid = res.user.uid;
+        this.db.collection('users').doc(uid).set({
+          nombre: nombre,
+          institucion: institucion,
+          uid: uid
+        })
+        resolve(res);
+      }).catch(err => rejected(err));
+    })
+
+  }
+
+  resetPassword(email: string){
+    return this.AFauth.sendPasswordResetEmail(email);
   }
 }
